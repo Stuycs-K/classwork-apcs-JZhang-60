93d47b0fe21d8db1f090ef806064fcfb39e540b0
Jeffery Zhang <jzhang60@stuy.edu>
2024-09-27 10:15:17 -0400

    added returnCopy and test case

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -11,11 +11,20 @@ public class MyArrays{
 
   public static int[] returnCopy(int[] ary)
   {
-    
+    int[] ary2 = new int[ary.length];
+    for(int i = 0; i < ary.length; i++)
+    {
+      ary2[i] = ary[i];
+    }
+    return ary2;
   }
+
   public static void main(String[] args)
   {
     int[] arrString = {1, 2, 3};
     System.out.println(arrayToString(arrString));
+
+    int[] copy = {1, 2, 3};
+    System.out.println(returnCopy(copy));
   }
 }
