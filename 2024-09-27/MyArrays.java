e0054d49016075d891abe593f7c922772e8de630
Jeffery Zhang <jzhang60@stuy.edu>
2024-09-27 10:03:08 -0400

    version 1 arrayToString

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,15 @@
+public class MyArrays{
+  public static String arrayToString(int[] ary)
+  {
+    string start = "[";
+    for(int i = 0; i < ary.length; i++)
+    {
+      start += ary[i];
+    }
+    return (start + "]");
+  }
+  public static void main(Strings[] args)
+  {
+    System.out.println(arrayToString);
+  }
+}
