5ee9e2b9df2f4e0f9a99310588550499b7f603e2
Jeffery Zhang <jzhang60@stuy.edu>
2024-09-27 20:54:32 -0400

    Added concatArray and its testcase

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -4,9 +4,12 @@ public class MyArrays{
     String start = "[";
     for(int i = 0; i < ary.length; i++)
     {
-      start += ary[i];
+		if(i < ary.length - 1)
+		{
+			start += (ary[i] + ", ");
+		}
     }
-    return (start + "]");
+    return (start + ary[ary.length - 1] + "]");
   }
 
   public static int[] returnCopy(int[] ary)
@@ -18,7 +21,28 @@ public class MyArrays{
     }
     return ary2;
   }
-
+  
+  public static int[] concatArray(int[]ary1,int[]ary2)
+  {
+	  int[] ary3 = new int[ary1.length + ary2.length];
+	  int arycounter = 0;
+	  for(int i = 0; i < (ary1.length + ary2.length); i++)
+	  {
+		  if(i < ary1.length)
+		  {
+			  ary3[i] = ary1[arycounter];
+			  arycounter++;
+		  }
+		  if(i >= ary1.length)
+		  {
+			  ary3[i] = ary2[arycounter - ary1.length];
+			  arycounter++;
+		  }
+	  }
+	  return ary3;
+  }
+	  
+	  
   public static void main(String[] args)
   {
     int[] arrString = {1, 2, 3};
@@ -27,6 +51,9 @@ public class MyArrays{
     int[] copy = {4, 5, 2};
     System.out.println("Are the arrays the same? " + (returnCopy(copy) == copy));
 	
-	
+	int[] concat1 = {2, 1, 9, 0};
+	int[] concat2 = {40, 1, 3, 4, 8, 2};
+	int[] concatResult = {2, 1, 9, 0, 40, 1, 3, 4, 8, 2};
+	System.out.println(arrayToString(concatArray(concat1, concat2)).equals(arrayToString(concatResult)));
   }
 }
