bdf9e3f0087ae1e1b1e47350d27d0b3f0ade0be7
Jeffery Zhang <jzhang60@stuy.edu>
2024-10-15 21:38:36 -0400

    Added more testcases to each method test case

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -14,15 +14,25 @@ public class Demo{
     Point p3 = new Point(3,4);
 	Point p4 = new Point(0, 0);
 	Point p5 = new Point(-4, 5);
+	
+	// Constructor test cases
     System.out.println( p3);
 	System.out.println( p5);
 	
+	// Static distance in Demo.java test cases
     System.out.println( distance(p1,p2));
 	System.out.println( distance(p4,p5));
+	System.out.println( distance(p3,p4));
 	
+	// Static distance in Point.java test cases
     System.out.println( Point.distance(p1,p2));
+	System.out.println( Point.distance(p4,p5));
+	System.out.println( Point.distance(p3,p4));
 	
+	// Non-Static in Point.java test cases
     System.out.println( p1.distanceTo(p2));
+	System.out.println( p4.distanceTo(p5));
 	System.out.println( p3.distanceTo(p4));
+	
   }
 }
