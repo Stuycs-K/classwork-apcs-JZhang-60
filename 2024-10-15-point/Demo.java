ea88d6baf4a656195f4156309988c01a347563ed
Jeffery Zhang <jzhang60@stuy.edu>
2024-10-15 21:52:26 -0400

    Added three Points for Equilateral Triangle test

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -34,5 +34,10 @@ public class Demo{
 	System.out.println( p4.distanceTo(p5));
 	System.out.println( p3.distanceTo(p4));
 	
+	// Equilateral Triangle objects
+	Point t = new Point(0, 0);
+	Point t1 = new Point(1, 0);
+	Point t2 = new Point(0.5, Math.sqrt(3)/2);
+	
   }
 }
