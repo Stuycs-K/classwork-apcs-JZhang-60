37d78eb338f09f9341db751715cd0096c8838a0b
Jeffery Zhang <jzhang60@stuy.edu>
2024-11-03 17:57:03 -0500

    Changed program to only print lines with {

--- 2024-10-31-Advent/ReadFile.java
+++ 2024-10-31-Advent/ReadFile.java
@@ -8,15 +8,22 @@ public class ReadFile {
   public static void main(String[] args) {
     //2 Opening a file requires a try/catch
     try {
-      File file = new File("ReadFile.java" );//1
+      File file = new File("ReadFile.java");//1
       Scanner input = new Scanner(file);
       //CODE THAT SCANS THE FILE.
-	  while(input.hasNextLine()){
+	  /*while(input.hasNextLine()){
           System.out.println(input.nextLine());
 		  if(input.hasNextLine()){
 			  input.nextLine();
 		  }
-	  }
+	  }*/
+	  while(input.hasNextLine()){
+		  String store = "";
+		  store = input.nextLine();
+		  if(store.indexOf("{") >= 0){
+		  System.out.println(store);
+		  }
+		  }
       input.close();//releases the file from your program
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
