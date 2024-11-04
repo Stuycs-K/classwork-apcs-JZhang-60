a2df933e9a62f98bdef10e8c480443f8e6999eca
Jeffery Zhang <jzhang60@stuy.edu>
2024-11-03 22:16:52 -0500

    Completed countTrianglesB

--- 2024-10-31-Advent/TriangleTester.java
+++ 2024-10-31-Advent/TriangleTester.java
@@ -23,8 +23,44 @@ public class TriangleTester{
 		}
 		return triangleCounter;
 	}
+	
+	public static int countTrianglesB(String filename){
+		int triangleCounter = 0;
+		try{
+			File file = new File(filename);
+			Scanner input = new Scanner(file);
+			while(input.hasNextLine()){
+				String store = "";
+				String store1 = "";
+				String store2 = "";
+				if(input.hasNextLine()){
+				store = input.nextLine();
+				}
+				if(input.hasNextLine()){
+				store1 = input.nextLine();
+				}
+				if(input.hasNextLine()){
+				store2 = input.nextLine();
+				}
+				Scanner inputstore = new Scanner(store);
+				Scanner inputstore1 = new Scanner(store1);
+				Scanner inputstore2 = new Scanner(store2);
+				for(int i = 0; i < 3; i++){
+					if(isTriangle(inputstore.nextInt(), inputstore1.nextInt(), inputstore2.nextInt())){
+					triangleCounter++;
+				}
+				}
+			}
+		}
+			catch(FileNotFoundException ex){
+				System.out.println("File not found");
+			}
+			return triangleCounter;
+		}
+
 	public static void main(String[] args){
 		System.out.println(countTrianglesA("inputTri.txt"));
+		System.out.println(countTrianglesB("inputTri.txt"));
 	}
 }
 		
