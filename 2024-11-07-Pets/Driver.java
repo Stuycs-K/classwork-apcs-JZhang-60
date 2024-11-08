f15950dce9b071ae031f5f448d3a9e38ceccc980
Jeffery Zhang <jzhang60@stuy.edu>
2024-11-08 08:03:52 -0500

    implemented overriden getName() and comment about it

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,6 +1,4 @@
-/*
-
-*/
+// Overriding the getName() method inherited the getName() of the Superclass, allowing us to modify the method accordingly to what we want our subclass to behave.
 public class Driver{
   public static void main(String[] args){
     Animal cat = new Animal("meow", 2, "sigma");
