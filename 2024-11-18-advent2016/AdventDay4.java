573fbf9c1de8fb0a175306a6d05755d7f4c112cd
Jeffery Zhang <jzhang60@stuy.edu>
2024-11-19 23:09:24 -0500

    Completed and debugged day 4

--- 2024-11-18-advent2016/AdventDay4.java
+++ 2024-11-18-advent2016/AdventDay4.java
@@ -7,17 +7,44 @@ public class AdventDay4{
     System.out.println(securityChecker("checker4.txt"));
   }
   public static int securityChecker(String fileName){
+	  int sum = 0;
+	  String freqFive = "";
   try{
     Scanner input = new Scanner(new File(fileName));
     while(input.hasNextLine()){
-      String[] store = input.nextLine().split("-");
-      System.out.println(store[5]);
-
-    }
+		String store = input.nextLine().replace("-", "");
+		String characters = store.substring(0, store.length()-10);
+		String ID = store.substring(store.length()-10, store.length()-7);
+		String checksum = store.substring(store.length()-6, store.length()-1);
+		String charactersRecurred = "";
+		//System.out.println(store.substring(store.length()-10, store.length()-7));
+		int[] freq = new int[26];
+		for(int i = 0; i < characters.length(); i++){
+			freq[characters.charAt(i) - 'a']++;
+		}
+		for(int i = 0; i<5;i++){
+			int maxRecurrence = 0;
+			char maxChar = ' ';
+			for(int j = 0; j <= 25; j++){
+				int cIndex = (char) (j+97) - 'a';
+				if(freq[cIndex] > maxRecurrence){
+					maxRecurrence = freq[cIndex];
+					maxChar = (char) (j+97);
+				}
+			}
+			charactersRecurred+=maxChar;
+			//System.out.println(charactersRecurred);
+			freq[maxChar-'a'] = -1;
+  }
+  if(charactersRecurred.equals(checksum)){
+	  sum+=Integer.valueOf(ID);
+  }
+	}
+	input.close();
   }
 catch(FileNotFoundException e){
   System.out.println("file not found");
 }
-return 0;
+return sum;
 }
 }
