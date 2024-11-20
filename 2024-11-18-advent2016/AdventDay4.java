aa349045231264bff243b89a18f680a719ab9e16
Jeffery Zhang <jzhang60@stuy.edu>
2024-11-19 23:52:08 -0500

    Completed part B and removed old variable from old broken code

--- 2024-11-18-advent2016/AdventDay4.java
+++ 2024-11-18-advent2016/AdventDay4.java
@@ -2,13 +2,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+//Part A
 public class AdventDay4{
   public static void main(String[] args){
     System.out.println(securityChecker("checker4.txt"));
+	System.out.println(securityCheckerB("checker4.txt"));
   }
   public static int securityChecker(String fileName){
 	  int sum = 0;
-	  String freqFive = "";
   try{
     Scanner input = new Scanner(new File(fileName));
     while(input.hasNextLine()){
@@ -47,4 +48,45 @@ catch(FileNotFoundException e){
 }
 return sum;
 }
+
+//Part B
+public static String securityCheckerB(String fileName){
+	  int sum = 0;
+  try{
+    Scanner input = new Scanner(new File(fileName));
+    while(input.hasNextLine()){
+		String store = input.nextLine().replace("-", "");
+		String characters = store.substring(0, store.length()-10);
+		String ID = store.substring(store.length()-10, store.length()-7);
+		String checksum = store.substring(store.length()-6, store.length()-1);
+		String deciphered = caesarCipher(characters, Integer.valueOf(ID));
+		if(deciphered.indexOf("object") > -1){
+			System.out.println(deciphered + ID);
+		}
+	}
+	input.close();
+  }
+catch(FileNotFoundException e){
+  System.out.println("file not found");
 }
+return "";
+}
+
+public static String caesarCipher(String input, int a){
+	String caesarResult = "";
+	for(int i = 0; i<input.length(); i++){
+		char currentChar = input.charAt(i);
+		for(int j = 0; j<a;j++){
+		if(currentChar == 'z'){
+			currentChar = 'a';
+		}
+		else{
+			currentChar++;
+		}
+	}
+	caesarResult+= currentChar + "";
+}
+return caesarResult;
+}
+}
+
