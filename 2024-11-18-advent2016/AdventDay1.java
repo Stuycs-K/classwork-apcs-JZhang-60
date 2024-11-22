3842ea24d8e6e2d7efc921bf486ee98cda3b3412
Jeffery Zhang <jzhang60@stuy.edu>
2024-11-21 22:31:38 -0500

    Completed and debugged Part2

--- 2024-11-18-advent2016/AdventDay1.java
+++ 2024-11-18-advent2016/AdventDay1.java
@@ -1,12 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class AdventDay1{
   public static void main(String[] args){
 	  System.out.println(blockResult("checker.txt"));
+	  System.out.println(blockResultB("checker.txt"));
   }
   public static int blockResult(String fileName){
-    String store = "";
     int xPos = 0;
     int yPos = 0;
     int tracker = 1; //1 = N, 2 = E, 3 = S, 4 = W
@@ -48,6 +49,56 @@ public class AdventDay1{
 		}
 	  }
 	}
+	}
+	input.close();
+	}
+	catch(FileNotFoundException e){
+		System.out.println("File not found");
+	}
+	return Math.abs(xPos) + Math.abs(yPos);
+  }
+  //Part B
+  public static int blockResultB(String fileName){
+    int xPos = 0;
+    int yPos = 0;
+    int tracker = 0; //0 = N, 1 = E, 2 = S, 3 = W
+	String visited = "(0,0)";
+	try{
+	File file = new File(fileName);
+    Scanner input = new Scanner(file);
+    while(input.hasNext()){
+		String store = input.next();
+		store = store.replace(",", "");
+		int value = Integer.valueOf(store.substring(1));
+		if(store.charAt(0) == 'R'){
+			tracker = (tracker+1)%4;
+		}
+		if(store.charAt(0) == 'L'){
+			tracker = (tracker-1+4)%4;
+		}
+		for(int i = 0; i<value;i++){
+			if(tracker == 0){
+				yPos++;
+			}
+			if(tracker == 1){
+				xPos++;
+			}
+			if(tracker == 2){
+				yPos--;
+			}
+			if(tracker == 3){
+				xPos--;
+			}
+			String current = "("+xPos+","+yPos+")";
+			//System.out.println(current);
+			//System.out.println(visited);
+		   if(visited.indexOf(current) > -1){
+			   return (Math.abs(xPos)+Math.abs(yPos));
+		   }else{
+		   visited += current;
+		   }
+	   }
+	  //System.out.println(current);
 	}
 	input.close();
 	}
