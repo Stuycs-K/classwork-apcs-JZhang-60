3dd1bc055a77ec1fe56cf5ab59f615696c5ee106
Jeffery Zhang <jzhang60@stuy.edu>
2024-11-21 22:32:40 -0500

    Completed and debugged Part2

--- 2024-11-18-advent2016/AdventDay2.java
+++ 2024-11-18-advent2016/AdventDay2.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class AdventDay2{
   public static void main(String[] args){
 	  System.out.println(keypadResult("checker2.txt"));
+	  System.out.println(keypadResultB("checker2.txt"));
   }
   public static int keypadResult(String fileName){
 	  int finalFinalCode = 0;
@@ -42,31 +43,61 @@ public class AdventDay2{
 	  }
 	  return finalFinalCode;
   }
-  //Part B
-    public static int keypadResultB(String fileName){
-  	  int finalFinalCode = 0;
+ //Part B
+    public static String keypadResultB(String fileName){
+  	  String finalFinalCode = "";
   	  int keypadNumPos = 5; // 10 = A 11 = B 12 = C 15 = D
-  	  ArrayList<Integer> finalCode = new ArrayList<Integer>();
+  	  ArrayList<String> finalCode = new ArrayList<String>();
   	  try{
   		  File file = new File(fileName);
   		  Scanner input = new Scanner(file);
   		  while(input.hasNextLine()){
   			  String store = input.nextLine();
   			  for(int i = 0; i < store.length(); i++){
-  			  if(store.charAt(i) == ('U') && keypadNumPos % ){
-  				  if(keypadNumPos -= 3
+  			  if(store.charAt(i) == ('U')){
+				  if(keypadNumPos == 3 || keypadNumPos == 13){
+					  keypadNumPos-=2;
+				  }
+				  if(keypadNumPos == 6 || keypadNumPos == 7 || keypadNumPos == 8 || keypadNumPos == 10 || keypadNumPos == 11 || keypadNumPos == 12){
+					  keypadNumPos-= 4;
+				  }
   			  } else
-  			  if(store.charAt(i) == ('D') && keypadNumPos < 7){
-  				  keypadNumPos += 3;
+  			  if(store.charAt(i) == ('D')){
+  				  if(keypadNumPos ==1 || keypadNumPos == 11){
+					  keypadNumPos += 2;
+				  }
+				  if(keypadNumPos == 2 || keypadNumPos == 3 || keypadNumPos == 4 || keypadNumPos == 6 || keypadNumPos == 7 || keypadNumPos == 8){
+					  keypadNumPos+= 4;
+				  }
   			  } else
-  			  if(store.charAt(i) == ('L') && keypadNumPos % 3 != 1){
-  				  keypadNumPos -= 1;
+  			  if(store.charAt(i) == ('L')){
+  				  if(keypadNumPos != 1 && keypadNumPos != 2 && keypadNumPos != 5 && keypadNumPos != 10 && keypadNumPos != 13){
+					  keypadNumPos--;
+				  }
   			  } else
-  			  if(store.charAt(i) == ('R') && keypadNumPos % 3 != 0){
-  				  keypadNumPos += 1;
+  			  if(store.charAt(i) == ('R')){
+  				  if(keypadNumPos != 1 && keypadNumPos != 4 && keypadNumPos != 9 && keypadNumPos != 12 && keypadNumPos != 13){
+					  keypadNumPos++;
+				  }
   			  }
   		  }
-  		  finalCode.add(keypadNumPos);
+		  //System.out.println(keypadNumPos);
+		  if(keypadNumPos <= 9){
+			  finalCode.add("" + keypadNumPos);
+		  }else
+		  if(keypadNumPos == 10){
+			  finalCode.add("A");
+		  }else
+		  if(keypadNumPos == 11){
+			  finalCode.add("B");
+		  }else
+		  if(keypadNumPos == 12){
+			  finalCode.add("C");
+		  }else
+		  if(keypadNumPos == 15){
+			  finalCode.add("D");
+		  }
+				  
   		  keypadNumPos = 5;
   		}
   		  input.close();
@@ -75,9 +106,8 @@ public class AdventDay2{
   		  System.out.println("File not found");
   	  }
   	  for(int i = 0; i<finalCode.size();i++){
-  		  finalFinalCode += finalCode.get(i) * (int) Math.pow(10, finalCode.size()-i-1);
+  		  finalFinalCode += finalCode.get(i);
   	  }
   	  return finalFinalCode;
     }
-  }
 }
--- 2024-11-18-advent2016/AdventDay6.java
+++ 2024-11-18-advent2016/AdventDay6.java
@@ -1,9 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+//Part A
 public class AdventDay6{
   public static void main(String[] args){
     System.out.println(radioChecker("checker6.txt"));
+	System.out.println(radioCheckerB("checker6.txt"));
   }
   public static char freqChar(String str){
 	  int[] alphabet = new int[26];
@@ -46,4 +48,46 @@ public class AdventDay6{
 	  }
   return finalChar;
   }
+  //Part B
+  public static char leastFreqChar(String str){
+	  int[] alphabet = new int[26];
+	  int leastRecurrence = 10000;
+	  char leastChar = ' ';
+	  for(int i = 0; i < str.length(); i++){
+		  int index = str.charAt(i) - 'a';
+		  alphabet[index]++;
+	  }
+	  for(int i = 0; i < 26; i++){
+		  if(alphabet[i] < leastRecurrence){
+			  leastRecurrence = alphabet[i];
+			  leastChar = (char) (i + 'a');
+		  }
+	  }
+	  return leastChar;
+  }
+  public static String radioCheckerB(String fileName){
+	  String finalChar = "";
+	  String[] columns = new String[8];
+	  for(int i = 0; i < columns.length; i++){
+		  columns[i] = "";
+	  }
+  try{
+	  Scanner input = new Scanner(new File(fileName));
+	  while(input.hasNextLine()){
+		  String store = input.nextLine();
+		  String indexPlacer = store.substring(0,8);
+		  for(int i = 0; i<8;i++){
+			  columns[i] += indexPlacer.charAt(i);
+		  }
+	  }
+	  input.close();
+  }
+	  catch(FileNotFoundException e){
+		  System.out.println("file not found");
+	  }
+	  for(int i = 0; i < columns.length; i++){
+			  finalChar += leastFreqChar(columns[i]);
+	  }
+	  return finalChar;
+}
 }
