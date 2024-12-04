025e97f8f03268737ac42396c4e95bf663f11cf8
Jeffery Zhang <jzhang60@stuy.edu>
2024-12-04 07:55:39 -0500

    updated to be more usable

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,27 +1,21 @@
 public class ColorDemo{
 	public static void main(String[] args){
-		int r = 100, b = 100, g = 100;
-		for(int i = 0; i < 500; i++){
-			if(r <= 256){
-				algo(r);
-			}
-			if(b <= 256){
-				b++;
-			}
-			if(g <= 256){
-				algo(g);
-			}
-		System.out.print("\u001b[38;2;"+r+";"+b+";"+g+";1mChristmas");
-		System.out.print("\u001b[38;2;"+b+";"+g+";"+r+";1mEve");
-  }
+		System.out.println("test");
+		color(100, 90, 200);
+		System.out.println("test2");
+		color2(100, 90, 200);
+		System.out.println("test3");
+		System.out.println("" + CLEAR_SCREEN);
+		System.out.println("\u001b[0m");
 }
- public static int algo(int num){
-	  if(num%2 == 0){
-		  return num /= 2;
-	  }
-	  if(num%2 == 1){
-		  return num = num*3+1;
-	  }
-	  return 0;
-  }
+public static final String CLEAR_SCREEN =  "\u001b[2J";
+public static final String HIDE_CURSOR =  "\u001b[?25l";
+public static final String SHOW_CURSOR =  "\u001b[?25h";
+public static void color(int r, int g, int b){
+	System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";1m");
+}
+
+public static void color2(int r, int g, int b){
+	System.out.print("\u001b[38;2;"+b+";"+g+";"+r+";1m");
+		}
 }
