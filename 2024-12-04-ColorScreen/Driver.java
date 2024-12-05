9b14e98c050238852f114f5bb529e266639992fa
Jeffery Zhang <jzhang60@stuy.edu>
2024-12-04 19:35:03 -0500

    Horizontal separator added

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -2,17 +2,19 @@ public class Driver{
 	public static void main(String[] args){
 		System.out.print(Text.CLEAR_SCREEN);
 		int[] threeInt = new int[3];
+		int w = 80;
+		int h = 30;
 		fillArr(threeInt);
 		Text.color(Text.BLUE,Text.background(Text.BLUE));
-		for(int i = 1; i <= 30; i++){
-			for(int j = 1; j <= 80; j++){
-				if(i == 1 || i == 30){
+		for(int i = 1; i <= h; i++){
+			for(int j = 1; j <= w; j++){
+				if(i == 1 || i == h){
 		Text.go(i, j);
 		System.out.print("1");
-				}else if(i > 1 && i < 30){
+				}else if(i > 1 && i < h){
 					Text.go(i, 1);
 					System.out.print("1");
-					Text.go(i, 80);
+					Text.go(i, w);
 					System.out.print("1");
 				}
 				}
@@ -22,6 +24,11 @@ public class Driver{
 			colorPicker(threeInt[i]);
 			System.out.print(threeInt[i]);
 		}
+		Text.color(Text.WHITE,Text.background(Text.BLUE),Text.BRIGHT);
+		for(int i = 2; i <= w-1; i++){
+			Text.go(3, i);
+			System.out.print("-");
+	}
 	}
 	public static int[] fillArr(int[] arr){
 		for(int i = 0; i < arr.length; i++){
