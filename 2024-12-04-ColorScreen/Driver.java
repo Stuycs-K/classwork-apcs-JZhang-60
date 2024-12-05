e776309fc3aadfc67c04154717067ba298f28c35
Jeffery Zhang <jzhang60@stuy.edu>
2024-12-04 19:05:15 -0500

    Added methods to fill array with random ints

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,5 +1,7 @@
 public class Driver{
 	public static void main(String[] args){
+		int[] threeInt = new int[3];
+		fillArr(threeInt);
 		Text.color(Text.BLUE,Text.background(Text.BLUE));
 		for(int i = 1; i <= 30; i++){
 			for(int j = 1; j <= 80; j++){
@@ -14,6 +16,13 @@ public class Driver{
 				}
 				}
 			}
+	}
+	public static int[] fillArr(int[] arr){
+		for(int i = 0; i < arr.length; i++){
+			arr[i] = (int) (Math.random() * 100);
+			System.out.println(arr[i]);
+		}
+		return arr;
 	}
 }
 	
