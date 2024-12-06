79cf61b2c93eb4ce0f04d7a0587acd7a281f62df
Jeffery Zhang <jzhang60@stuy.edu>
2024-12-05 21:34:01 -0500

    Implemented Warrior subclass and concrete methods

---
+++ 2024-12-06-Adventurer/Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+	public static void main(String[] args){
+		Adventurer p1 = new Warrior("Bob",10);
+		System.out.println(p1.getName());
+	}
+}
---
+++ 2024-12-06-Adventurer/Warrior.java
@@ -0,0 +1,56 @@
+public class Warrior extends Adventurer{
+	private int rage;
+	private int maxRage;
+	
+	public Warrior(String name, int hp){
+	super(name, hp);
+	}
+	
+	@Override
+	public String getSpecialName(){
+		return "Rage";
+	}
+	@Override
+	public int getSpecial(){
+		return rage;
+	}
+	
+	@Override
+	public void setSpecial(int n){
+		rage = n;
+	}
+		
+	@Override
+	public int getSpecialMax(){
+		return maxRage;
+	}
+	
+	@Override
+	public String attack(Adventurer other){
+		other.applyDamage(30);
+		return this.getName() + "hit the opponent with a slash!";
+	}
+		
+	@Override
+	public String support(Adventurer other){
+		other.setHP(getHP()+20);
+		return "Bandaged! " + other.getName() + " is now at " + this.getHP()+ "!";
+	}
+		
+	@Override
+	public String support(){
+		this.setHP(getHP()+20);
+		return "Bandaged! " + this.getName() + " is now at " + this.getHP() + "!";
+	}
+	
+	@Override
+	public String specialAttack(Adventurer other){
+		if(rage<30){
+			return this.getName() + "needs more" + this.getSpecialName() + "to perform special attack!";
+		}
+		rage-=30;
+		other.applyDamage(75);
+		return this.getName() + "consumed all their" + this.getSpecialName() + "and charged at the opponent!";
+	}
+}
+	
