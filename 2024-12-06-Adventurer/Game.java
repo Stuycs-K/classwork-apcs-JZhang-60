38c1b510818eed3c328b0f70648c39f1211a0dd1
Jeffery Zhang <jzhang60@stuy.edu>
2024-12-09 10:11:41 -0500

    Attempt at implementing name prints

---
+++ 2024-12-06-Adventurer/Game.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    //do this once
+    Scanner userInput = new Scanner(System.in);
+
+    //You can do the rest many times:
+
+    System.out.println("Enter username");
+    //Read one line of user input
+    String userName = userInput.nextLine();
+    //Do something with the input
+    System.out.println("Username is: " + userName);
+    while(userInput.hasNextLine()){
+      System.out.println(getName()+", " getHP() + "/" + getmaxHP() + ", " + getSpecial() + "/" + getSpecialMax());
+    }
+  }
+}
