629bdefa06bf7353b561f56e727cfcdaa83aed54
Jeffery Zhang <jzhang60@stuy.edu>
2024-12-09 12:20:02 -0500

    Implemented nameprinting, option prompts, and input actions

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+	  boolean programSwitch = true;
+	  Adventurer player = new Warrior("Conan", 16);
+	  Adventurer enemy = new CodeWarrior("Codex", 12);
     //do this once
     Scanner userInput = new Scanner(System.in);
-
-    //You can do the rest many times:
-
-    System.out.println("Enter username");
-    //Read one line of user input
-    String userName = userInput.nextLine();
-    //Do something with the input
-    System.out.println("Username is: " + userName);
-    while(userInput.hasNextLine()){
-      System.out.println(getName()+", " getHP() + "/" + getmaxHP() + ", " + getSpecial() + "/" + getSpecialMax());
+    while(programSwitch){
+      System.out.println(player.getName()+", " +player.getHP() + "/" + player.getmaxHP() + ", " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName());
+	  System.out.println(enemy.getName()+", " +enemy.getHP() + "/" + enemy.getmaxHP() + ", " + enemy.getSpecial() + "/" + enemy.getSpecialMax() + " " + enemy.getSpecialName());
+	  System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+	  String playerInput = userInput.nextLine();
+	  if(playerInput.equals("a") || playerInput.equals("attack")){
+		  player.attack(enemy);
+    } else if(playerInput.equals("s") || playerInput.equals("special")){
+		  player.specialAttack(enemy);
+    } else if(playerInput.equals("su") || playerInput.equals("support")){
+		  player.support();
+    } else if(playerInput.equals("quit")){
+		  programSwitch = true;
     }
-  }
+}
+}
 }
